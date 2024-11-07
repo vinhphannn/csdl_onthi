805e0eb1f81e3addc7b379a209c2eee1a08aae2d
vinh <info.vinhphan@gmail.com>
2024-11-07 23:03:14 +0700

    them lan dau 2

---
+++ src/on_thi_csdl2/csdl.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package on_thi_csdl2;
+
+/**
+ *
+ * @author VInh
+ */
+public class csdl {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        GUI gd=new GUI();
+        gd.initUI();
+        gd.loadData();
+        gd.loadComboboxData();
+    }
+    
+}
